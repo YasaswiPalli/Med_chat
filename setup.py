16ffd833f1bd17875f4fdaa571bc9eb58df3ad63
yasaswipalli9@gmail.com <yasaswi@gmail.com>
2026-02-28 11:47:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptool import find_packages, setup
+
+setup(
+    name="Med_chat",
+    version="0.1.0",
+    authoer="Med_chat Team",
+    author_email="<yasaswipalli@gmail.com>",
+    packages=find_packages(),
+    install_requires=[]
+)
