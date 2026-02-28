80af66f3bce36905c8fbbf7aaa78a7ca62cf3fd9
yasaswipalli9@gmail.com <yasaswi@gmail.com>
2026-02-28 10:38:32 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch  src/__init__.py
+touch src/healper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requiremenrts.txt
+
+
+echo "directory and files created successfully!"
